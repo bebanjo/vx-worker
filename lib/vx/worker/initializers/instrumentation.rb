d7d08dfd0b27cd45e33c8ab6f4fe5f9cb3d385b8
Dmitry Galinsky <dima.exe@gmail.com>
2014-06-07 06:50:42 +0400

    ignore JobLogsConsumer in instrumentation

--- lib/vx/worker/initializers/instrumentation.rb
+++ lib/vx/worker/initializers/instrumentation.rb
@@ -1,11 +1,18 @@
 require 'vx/instrumentation'
 require 'active_support/notifications'
 
-
 Vx::Instrumentation.install Vx::Worker.root.join("log/vxworker.log.json")
 
 $stdout.puts ' --> initializing ActiveSupport::Notifications'
 
 ActiveSupport::Notifications.subscribe(/.*/) do |event, started, finished, _, payload|
-  Vx::Instrumentation.delivery event, payload, event.split("."), started, finished
+  ignored = false
+
+  if event == 'process_publishing.consumer.vx'
+    ignored = (payload[:consumer] == 'Vx::Worker::JobLogsConsumer')
+  end
+
+  unless ignored
+    Vx::Instrumentation.delivery event, payload, event.split("."), started, finished
+  end
 end
